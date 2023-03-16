2fd4ecc8f00f3998653f9aba3d66fcefdc163ab7
MicheleRai <rainonemichele1999@gmail.com>
2023-03-16 14:15:28 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿snack1();
+//snack2();
+
+void snack1()
+{
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    int sum = num1 - num2;
+
+    if (num1 == num2)
+    {
+        Console.WriteLine("Numbers are equal");
+    }
+    else
+    {
+        Console.WriteLine(sum);
+    }
+}
+
+void snack2()
+{
+    string str1 = Console.ReadLine() ?? "";
+    string str2 = Console.ReadLine() ?? "";
+
+    if (str1.Length > str2.Length)
+    {
+        Console.WriteLine(str1);
+        Console.WriteLine(str2);
+    }
+    else
+    {
+        Console.WriteLine(str2);
+        Console.WriteLine(str1);
+    }
+}
